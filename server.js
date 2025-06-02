b41194ef56170c8510ca14ab5e1e791fa71d738a
hanrequit <hanreeleveld9@gmail.com>
2025-06-02 10:45:23 +0200

    sd

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const fs = require('fs/promises');
-const cors = require('cors');
 const path = require('path');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const cors = require('cors');
